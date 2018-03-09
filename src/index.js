b6655df2be58b1d1cface453e960659db649f69c
Anthony <anthonyjlower@gmail.com>
2018-03-09 08:52:12 -0600

    Testing new socket connection

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import io from 'socket.io-client';
 
-export const socket = io.connect('https://calcutta-socket.herokuapp.com/4000')
+export const socket = io.connect('https://calcutta-socket.herokuapp.com/')
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
